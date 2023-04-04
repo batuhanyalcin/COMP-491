d0bbbb95b8b10f548eb08a0ce6057649a04470f9
gsari18 <gsari18@ku.edu.tr>
2023-04-04 17:47:05 +0300

    Separated test result screen and added additional parameters

--- app/Screens/TestResult.js
+++ app/Screens/TestResult.js
@@ -1,12 +1,73 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import SpaghettiGraph from '../Models/SpaghettiGraph'
 
-export default function TestResult() {
+export default function TestResult({navigation, route}) {
+  const accX = route.params.accX
+  const accZ = route.params.accZ
+  const duration = route.params.duration
+  const dt = route.params.dt
+
+  function calculatePathLength(x, y) {
+    var pathLength = 0;
+    for (var i = 1; i < x.length; i++) {
+      pathLength += Math.sqrt(
+        (x[i] - x[i-1]) ** 2 +
+        (y[i] - y[i-1]) ** 2
+      )
+    }
+    return pathLength
+  }
+
+  function calculateJerk(x, y) {
+    // Calculate derivative of x and y
+    jerkX = Array(x.length)
+    // Forward FD for left end node
+    jerkX[0] = (-x[2] + 4*x[1] - 3*x[0]) / (2 * dt)
+    
+    // Central FD for internal nodes
+    var i
+    for (i = 1; i < x.length - 1; i++) {
+      jerkX[i] = (x[i+1] - x[i-1]) / (2 * dt)
+    }
+
+    // Backward FD for right end node
+    jerkX[i] = (-x[i-2] + 4*x[i-1] - 3*x[i]) / (2 * dt)
+    
+    // Take square of jerk
+    jerkX2 = jerkX.map(a => a**2)
+
+    // Integrate with Simpson's 1/3 Rule
+    var jerkI = 0
+    jerkI = jerkX2[0]
+    //console.log(jerkX2)
+    //console.log(jerkX[jerkX.length-2])
+    for (i = 1; i < x.length - 1; i++) {
+      jerkI += (i % 2 == 1 ? 4 * jerkX2[0] : 2 * jerkX2[0])
+    }
+    
+    jerkI += jerkX2[i]
+    jerkI *= dt / 3
+    return jerkI
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>TestResult</Text>
+      <SpaghettiGraph x={accX} y={accZ}/>
+      <Text>Metrics:</Text>
+      <Text>Path Length: {calculatePathLength(accX, accZ)}</Text>
+      <Text>Normalized Path Length: {duration / calculatePathLength(accX, accZ)}</Text>
+      <Text>Jerk: {calculateJerk(accX, accZ)}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- app/Screens/balanceTestScreen.js
+++ app/Screens/balanceTestScreen.js
@@ -5,7 +5,7 @@ import * as Speech from 'expo-speech'
 import { DeviceMotion } from 'expo-sensors';
 import SpaghettiGraph from '../Models/SpaghettiGraph';
 
-export default function balanceTestScreen() {
+export default function balanceTestScreen({navigation,route}) {
   const [data, setData] = useState({});
   const accX = [];
   const accY = [];
@@ -31,6 +31,7 @@ export default function balanceTestScreen() {
         Speech.speak("The test has ended", {language: 'en'})
         setGraphComp(<SpaghettiGraph x={accX} y={accZ}/>)
         setTestEnded(true)
+        navigation.navigate('TestResult', {accX: accX, accZ: accZ, duration: totalTime, dt: dt})
         return
       }
       i++
