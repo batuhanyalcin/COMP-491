e953d8f01c9641cf276c99af55749bb182b4aee0
Mel Chelou <melchelou@Mel-MacBook-Air.local>
2023-05-23 01:49:30 +0300

    timer countdown fixed

--- app/Screens/BalanceTestScreen.js
+++ app/Screens/BalanceTestScreen.js
@@ -1,48 +1,34 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Button} from 'react-native';
 import * as Speech from 'expo-speech'
 import { DeviceMotion } from 'expo-sensors';
 import { CountdownCircleTimer, useCountdown } from 'react-native-countdown-circle-timer';
 import Svg, { Path, LinearGradient, Stop, Defs } from 'react-native-svg';
-
+//import Button from '../components/Button'
+const accX = [];
+const accY = [];
+const accZ = [];
 export default function BalanceTestScreen({navigation,route}) {
   const [data, setData] = useState({});
-  const accX = [];
-  const accY = [];
-  const accZ = [];
+
   //const [graphComp, setGraphComp] = useState(<View></View>)
   //const [subscription, setSubscription] = useState(null);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
   const dt = 0.01 // seconds
-  const totalTime = 30 // seconds
+  const totalTime = 10 // seconds
   const numOfIterations = Math.floor(totalTime / dt)
   var i = 0
-  var isRemainingTimeSpeaking = false
+  const [isRemainingTimeSpeaking, setIsRemainingTimeSpeaking] = useState(false)
 
   const [time, setTime] = useState(3)
   const timerRef = useRef(time)
   DeviceMotion.setUpdateInterval(dt * 1000)
 
   const [testEnded, setTestEnded] = useState(false)
-
+  
   const _subscribe = () => {
     DeviceMotion.addListener((devicemotionData) => {
       setData(devicemotionData.acceleration);
-      let remainingTime = totalTime - Math.floor(i * dt)
-      if (remainingTime == 5 && !isRemainingTimeSpeaking) {
-        isRemainingTimeSpeaking = true
-        Speech.speak("5 seconds remaining.", {language: 'en'})
-      }
-      setTime(remainingTime.toString())
-      if (i == numOfIterations) {
-        _unsubscribe();
-        Speech.speak("The test has ended", {language: 'en'})
-        console.log(`Length of accX is: ${accX.length}`)
-        //setGraphComp(<SpaghettiGraph x={accX} y={accZ}/>)
-        setTestEnded(true)
-        navigation.replace('TestResult', {accX: accX, accZ: accZ, duration: totalTime, dt: dt})
-        return
-      }
-      i++
       accX.push(devicemotionData.acceleration.x)
       accY.push(devicemotionData.acceleration.y)
       accZ.push(devicemotionData.acceleration.z)
@@ -51,6 +37,7 @@ export default function BalanceTestScreen({navigation,route}) {
   };
   
   function startTest() {
+    setButtonDisabled(true)
     setTime(3)
     timerRef.current = 3
     
@@ -84,6 +71,18 @@ export default function BalanceTestScreen({navigation,route}) {
     })
   }
 
+  function onTestEnded() {
+    _unsubscribe();
+    
+    Speech.speak("The test has ended", {language: 'en'})
+    console.log("Printed accX")
+    console.log(`Length of accX is: ${accX.length}`)
+    //setGraphComp(<SpaghettiGraph x={accX} y={accZ}/>)
+    setTestEnded(true)
+
+    navigation.replace('TestResult', {accX: accX, accZ: accZ, duration: totalTime, dt: dt})
+  }
+
   const _unsubscribe = () => {
     DeviceMotion.removeAllListeners();
   };
@@ -91,8 +90,9 @@ export default function BalanceTestScreen({navigation,route}) {
   useEffect(() => {
     _subscribe();
     return () => _unsubscribe();
-  }, []);
+  }, []);s
   */
+
 
   const [isPlaying, setIsPlaying] = React.useState(false)
   const duration = totalTime
@@ -109,40 +109,31 @@ export default function BalanceTestScreen({navigation,route}) {
   
   return (
     <View style={styles.container}>
-      {true ? 
       <View style={{ width: size, height: size, position: 'relative' }}>
-        <Svg width={size} height={size}>
-          <Defs>
-            <LinearGradient id="your-unique-id" x1="1" y1="0" x2="0" y2="0">
-              <Stop offset="5%" stopColor="blue"/>
-              <Stop offset="95%" stopColor="purple"/>
-            </LinearGradient>
-          </Defs>
-          <Path
-            d={path}
-            fill="none"
-            stroke="#d9d9d9"
-            strokeWidth={strokeWidth}
-          />
-          {elapsedTime !== duration && (
-            <Path
-              d={path}
-              fill="none"
-              stroke={stroke}
-              strokeLinecap="round"
-              strokeWidth={strokeWidth}
-              strokeDasharray={pathLength}
-              strokeDashoffset={strokeDashoffset}
-            />
-          )}
-        </Svg>
-        <View style={styles.time}>
-          <Text style={{ fontSize: 36, fontWeight: 600, color: 'rgb(20,20,20)' }}>{time}</Text>
-        </View>
-      </View>
-      : null
+        <CountdownCircleTimer
+        isPlaying={isPlaying}
+        duration={totalTime}
+        colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+        colorsTime={[10, 6, 3, 0]}
+        onComplete={onTestEnded}
+    >
+      {({ remainingTime, color }) => {
+        if (remainingTime == 5 && !isRemainingTimeSpeaking) {
+          console.log(isRemainingTimeSpeaking)
+          setIsRemainingTimeSpeaking(true)
+          console.log(isRemainingTimeSpeaking)
+          Speech.speak("5 seconds remaining.", {language: 'en'})
+        }
+        return (
+        <Text style={{ color, fontSize: 40 }}>
+          {remainingTime}
+        </Text>
+        )
       }
-      <Button title='Start Test' onPress={startTest}></Button>
+    }
+    </CountdownCircleTimer>
+      </View>
+      <Button title='Start Test' style={styles.button} onPress={startTest} disabled={buttonDisabled}></Button>
     </View>
   );
 }
@@ -182,5 +173,9 @@ const styles = StyleSheet.create({
     top: 0,
     width: '100%',
     height: '100%'
+  },
+  button: {
+    borderRadius: 20
+
   }
 });
