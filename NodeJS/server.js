3a6e207203bdc7f7ad82aade76a55bc42ec057bd
EraySozer <79605926+EraySozer@users.noreply.github.com>
2023-05-29 16:12:52 +0300

    nothing changed

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const app=express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 var mysql = require('mysql');
 // create a connection variable with the required details
 var con = mysql.createConnection({
